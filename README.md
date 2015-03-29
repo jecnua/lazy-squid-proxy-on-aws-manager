edd501e111dc9a01d82262d632736278084def0c
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2015-03-29 15:56:02 +0100

    Added the function to get my ip from aws site

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ A lazy approach to manage my squid proxy
 - A working AWS ec2 account
 - Create a file called settings.py and fill the data like showed in
 settings.py.dist
+
+##Use case
+
+- My dynamic ip changed
---
+++ operations.py
@@ -0,0 +1,15 @@
+import boto.ec2
+import requests
+
+def stop(conn, id):
+    #conn.stop_instances(id)
+    pass
+
+def get_my_ip():
+    session = requests.session()
+    r = session.get('http://checkip.amazonaws.com/')
+    ip = r.text
+    return ip
+
+def add_my_ip(conn, id):
+    pass
