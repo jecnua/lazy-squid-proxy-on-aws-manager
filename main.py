ff29e7ceaa3e1e448d8be0d6adc850ecccb39c3b
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2015-03-29 17:31:27 +0100

    Remove the operation selection since it does only one thing.

--- main.py
+++ main.py
@@ -24,44 +24,44 @@ def run(region, squid_port='8888'):
                     instance.tags[remote_tag_name],
                     instance.state)
                 if instance.state == 'running':
-                    input_var = raw_input(
-                        'want me to stop(s)/restart(r)/addip(a)?: ')
-                    print ("You choose {}!".format(input_var))
-                    if input_var == 'a':
-                        sg = instance.groups[0]
-                        name = sg.name
-                        # FIXME: Maybe there is another way...
-                        groups = conn.get_all_security_groups()
-                        for group in groups:
-                            if group.name == name:
-                                print (
-                                    'Found the security group: {}!'.format(
-                                        group.name))
-                                ip = get_my_ip()
-                                fullrule = '{}/32'.format(ip)
-                                for rule in group.rules:
-                                    if rule.from_port == squid_port:
-                                        # Dirty but needed
-                                        if fullrule.rstrip() == str(
-                                                rule.grants[0]):
-                                            print 'Rule is already in place'
-                                            isNecessaryToAdd = False
-                                        else:
-                                            print 'I found an old rule for the \
-                                             port: {}. Removing.'.format(
-                                                rule.grants[0])
-                                            group.revoke(
-                                                rule.ip_protocol,
-                                                rule.from_port,
-                                                rule.to_port,
-                                                rule.grants[0])
-                                if isNecessaryToAdd:
-                                    print 'Adding {}'.format(ip)
-                                    group.authorize(
-                                        'tcp',
-                                        squid_port,
-                                        squid_port,
-                                        '{}/32'.format(ip))
+                    # input_var = raw_input(
+                    #     'want me to stop(s)/restart(r)/addip(a)?: ')
+                    # print ("You choose {}!".format(input_var))
+                    # if input_var == 'a':
+                    sg = instance.groups[0]
+                    name = sg.name
+                    # FIXME: Maybe there is another way...
+                    groups = conn.get_all_security_groups()
+                    for group in groups:
+                        if group.name == name:
+                            print (
+                                'Found the security group: {}!'.format(
+                                    group.name))
+                            ip = get_my_ip()
+                            fullrule = '{}/32'.format(ip)
+                            for rule in group.rules:
+                                if rule.from_port == squid_port:
+                                    # Dirty but needed
+                                    if fullrule.rstrip() == str(
+                                            rule.grants[0]):
+                                        print 'Rule is already in place'
+                                        isNecessaryToAdd = False
+                                    else:
+                                        print 'I found an old rule for the \
+                                         port: {}. Removing.'.format(
+                                            rule.grants[0])
+                                        group.revoke(
+                                            rule.ip_protocol,
+                                            rule.from_port,
+                                            rule.to_port,
+                                            rule.grants[0])
+                            if isNecessaryToAdd:
+                                print 'Adding {}'.format(ip)
+                                group.authorize(
+                                    'tcp',
+                                    squid_port,
+                                    squid_port,
+                                    '{}/32'.format(ip))
                     else:
                         pass
                 else:
